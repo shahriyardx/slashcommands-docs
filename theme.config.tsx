9a7e9d01eb4d7531dbe54f0a636cc2b45cc554f5
Md Shahriyar Alam <contact@shahriyar.dev>
2023-11-10 01:17:08 +0600

    added some functions and actions of member and server

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,12 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Slash Commands</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://ccbot.app',
   },
   chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
+    link: 'https://discord.gg/GZxrdAcT3w',
   },
 }
 
